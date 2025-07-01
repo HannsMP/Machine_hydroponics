d032ea1f65e54ba21313b90a1472d65a4f0b26e5
= <=>
2025-07-01 14:13:33 -0500

    0.0.5

---
+++ esp/code.preferences.mqtt.ino
@@ -0,0 +1,258 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "Adafruit_VEML7700.h"
+#include <PID_v1.h>
+#include <ArduinoJson.h>
+#include <Preferences.h>
+
+Preferences preferences;
+Adafruit_VEML7700 veml = Adafruit_VEML7700();
+
+// Pines
+const int LSL_PIN = 39;
+const int LSH_PIN = 36;
+const int EN[4]  = {19, 4, 26, 13};
+const int IN1[4] = {18, 17, 25, 14};
+const int IN2[4] = {5, 16, 33, 27};
+const int TDS_PIN = 34;
+const int PH_PIN = 35;
+const int TEMP_PIN = 32;
+const int PWM_LIGHT_PIN = 23;
+const int AIR_PUMP_PIN = 3;
+
+// Variables persistentes
+int COIL_AIR_PUMP;
+int COIL_BOMBA[4];
+
+int HREG_LIGHT_PWM;
+int HREG_BOMBA[4];
+int HREG_MODE;
+int HREG_LUX_SP;
+int HREG_AIR_ON_TIME, HREG_AIR_OFF_TIME;
+int HREG_B1_SP, HREG_B1_ON_TIME, HREG_B1_OFF_TIME;
+int HREG_B2_SP, HREG_B2_ON_TIME, HREG_B2_OFF_TIME;
+int HREG_B3_SP, HREG_B3_ON_TIME, HREG_B3_OFF_TIME;
+
+// Sensores
+int IREG_TDS_RAW, IREG_PH_RAW, IREG_TEMP_RAW;
+int IREG_LIGHT_LUX, IREG_LSL, IREG_LSH;
+
+// PID
+double lux, pwmLight, setpoint;
+PID LUX_PID(&lux, &pwmLight, &setpoint, 0.1, 0.5, 0, DIRECT);
+
+// TIMERS
+unsigned long t_now, t_last_air = 0, t_last_bomba[3] = {0,0,0};
+bool air_state = false, bomba_state[3] = {false, false, false};
+
+// WiFi y MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+  
+  preferences.begin("controller", false);
+  loadPreferences();
+
+  WiFi.begin("REHF-2.4G", "tontosYtorpes291");
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  Serial.println("\nWiFi conectado. IP:"); Serial.println(WiFi.localIP());
+
+  client.setServer("192.168.4.4", 1883);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    if (client.connect("ESP32Controller")) {
+      client.subscribe("controller/#");
+      client.subscribe("controller/get_state");
+    } else { delay(500); }
+  }
+
+  if (!veml.begin()) { while (1); }
+  veml.setLowThreshold(10000);
+  veml.setHighThreshold(20000);
+  veml.interruptEnable(true);
+
+  LUX_PID.SetOutputLimits(0, 255);
+  LUX_PID.SetMode(AUTOMATIC);
+
+  for (int i=0;i<4;i++) {
+    pinMode(EN[i], OUTPUT); pinMode(IN1[i], OUTPUT); pinMode(IN2[i], OUTPUT);
+  }
+  pinMode(AIR_PUMP_PIN, OUTPUT);
+  pinMode(LSL_PIN, INPUT);
+  pinMode(LSH_PIN, INPUT);
+}
+
+void loop() {
+  client.loop();
+  t_now = millis();
+
+  IREG_TDS_RAW = analogRead(TDS_PIN);
+  IREG_PH_RAW = analogRead(PH_PIN);
+  IREG_TEMP_RAW = analogRead(TEMP_PIN);
+  lux = veml.readLux();
+  IREG_LIGHT_LUX = (int)lux;
+  IREG_LSL = digitalRead(LSL_PIN);
+  IREG_LSH = digitalRead(LSH_PIN);
+
+  if ((HREG_MODE == 1) && (IREG_LSL==0)) controlAutomatico(IREG_TDS_RAW, IREG_PH_RAW);
+  else controlManual();
+}
+
+// CONTROL MANUAL
+void controlManual() {
+  for (int i=0;i<4;i++) {
+    if (COIL_BOMBA[i] && HREG_BOMBA[i] > 0) {
+      analogWrite(EN[i], HREG_BOMBA[i]);
+      digitalWrite(IN1[i], HIGH);
+      digitalWrite(IN2[i], LOW);
+    } else {
+      analogWrite(EN[i], 0);
+      digitalWrite(IN1[i], LOW);
+      digitalWrite(IN2[i], LOW);
+    }
+  }
+  digitalWrite(AIR_PUMP_PIN, COIL_AIR_PUMP);
+  analogWrite(PWM_LIGHT_PIN, HREG_LIGHT_PWM);
+}
+
+// CONTROL AUTOMATICO
+void controlAutomatico(int tds, int ph) {
+  setpoint = HREG_LUX_SP;
+  LUX_PID.Compute();
+  analogWrite(PWM_LIGHT_PIN, (int)pwmLight);
+
+  if (air_state && (t_now - t_last_air >= HREG_AIR_ON_TIME*1000)) {
+    air_state = false; t_last_air = t_now;
+  } else if (!air_state && (t_now - t_last_air >= HREG_AIR_OFF_TIME*1000)) {
+    air_state = true; t_last_air = t_now;
+  }
+  digitalWrite(AIR_PUMP_PIN, air_state);
+
+  int sp[3]={HREG_B1_SP,HREG_B2_SP,HREG_B3_SP};
+  int on[3]={HREG_B1_ON_TIME*1000,HREG_B2_ON_TIME*1000,HREG_B3_ON_TIME*1000};
+  int off[3]={HREG_B1_OFF_TIME*1000,HREG_B2_OFF_TIME*1000,HREG_B3_OFF_TIME*1000};
+
+  for (int i=0;i<3;i++) {
+    bool cond = (i<2) ? (tds<sp[i]) : (ph<sp[i]);
+    if (bomba_state[i] && (t_now - t_last_bomba[i] >= on[i])) {
+      bomba_state[i]=false; t_last_bomba[i]=t_now;
+    } else if (!bomba_state[i] && cond && (t_now - t_last_bomba[i] >= off[i])) {
+      bomba_state[i]=true; t_last_bomba[i]=t_now;
+    }
+    int pwm = bomba_state[i]?255:0;
+    analogWrite(EN[i], pwm);
+    digitalWrite(IN1[i], bomba_state[i]);
+    digitalWrite(IN2[i], LOW);
+  }
+}
+
+// MQTT
+void callback(char* topic, byte* payload, unsigned int length) {
+  payload[length] = '\0';
+  String t = topic;
+  String msg = String((char*)payload);
+
+  if (t == "controller/COIL_AIR_PUMP") { 
+    COIL_AIR_PUMP = msg.toInt();
+    preferences.putInt("COIL_AIR_PUMP", COIL_AIR_PUMP); 
+  }
+  else if (t == "controller/HREG_LIGHT_PWM") { 
+    HREG_LIGHT_PWM = msg.toInt();
+    preferences.putInt("HREG_LIGHT_PWM", HREG_LIGHT_PWM); 
+  }
+  else if (t == "controller/HREG_MODE") { 
+    HREG_MODE = msg.toInt();
+    preferences.putInt("HREG_MODE", HREG_MODE); 
+  }
+  else if (t == "controller/HREG_LUX_SP") { 
+    HREG_LUX_SP = msg.toInt();
+    preferences.putInt("HREG_LUX_SP", HREG_LUX_SP); 
+  }
+  else if (t == "controller/HREG_AIR_ON_TIME") { 
+    HREG_AIR_ON_TIME = msg.toInt();
+    preferences.putInt("HREG_AIR_ON_TIME", HREG_AIR_ON_TIME); 
+  }
+  else if (t == "controller/HREG_AIR_OFF_TIME") { 
+    HREG_AIR_OFF_TIME = msg.toInt();
+    preferences.putInt("HREG_AIR_OFF_TIME", HREG_AIR_OFF_TIME); 
+  }
+
+  else if (t == "controller/HREG_B1_SP") { 
+    HREG_B1_SP = msg.toInt();
+    preferences.putInt("HREG_B1_SP", HREG_B1_SP); 
+  }
+  else if (t == "controller/HREG_B1_ON_TIME") { 
+    HREG_B1_ON_TIME = msg.toInt();
+    preferences.putInt("HREG_B1_ON_TIME", HREG_B1_ON_TIME); 
+  }
+  else if (t == "controller/HREG_B1_OFF_TIME") { 
+    HREG_B1_OFF_TIME = msg.toInt();
+    preferences.putInt("HREG_B1_OFF_TIME", HREG_B1_OFF_TIME); 
+  }
+
+  else if (t == "controller/HREG_B2_SP") { 
+    HREG_B2_SP = msg.toInt();
+    preferences.putInt("HREG_B2_SP", HREG_B2_SP); 
+  }
+  else if (t == "controller/HREG_B2_ON_TIME") { 
+    HREG_B2_ON_TIME = msg.toInt();
+    preferences.putInt("HREG_B2_ON_TIME", HREG_B2_ON_TIME); 
+  }
+  else if (t == "controller/HREG_B2_OFF_TIME") { 
+    HREG_B2_OFF_TIME = msg.toInt();
+    preferences.putInt("HREG_B2_OFF_TIME", HREG_B2_OFF_TIME); 
+  }
+
+  else if (t == "controller/HREG_B3_SP") { 
+    HREG_B3_SP = msg.toInt();
+    preferences.putInt("HREG_B3_SP", HREG_B3_SP); 
+  }
+  else if (t == "controller/HREG_B3_ON_TIME") { 
+    HREG_B3_ON_TIME = msg.toInt();
+    preferences.putInt("HREG_B3_ON_TIME", HREG_B3_ON_TIME); 
+  }
+  else if (t == "controller/HREG_B3_OFF_TIME") { 
+    HREG_B3_OFF_TIME = msg.toInt();
+    preferences.putInt("HREG_B3_OFF_TIME", HREG_B3_OFF_TIME); 
+  }
+
+  else if (t == "controller/get_state") {
+    StaticJsonDocument<512> doc;
+    doc["TDS"]=IREG_TDS_RAW;
+    doc["PH"]=IREG_PH_RAW; 
+    doc["TEMP"]=IREG_TEMP_RAW;
+    doc["LUX"]=IREG_LIGHT_LUX; 
+    doc["LSL"]=IREG_LSL; 
+    doc["LSH"]=IREG_LSH;
+    doc["AIR"]=COIL_AIR_PUMP; 
+    doc["PWM_LIGHT"]=HREG_LIGHT_PWM;
+
+    char out[512]; serializeJson(doc, out);
+    
+    client.publish("controller/state", out);
+  }
+}
+
+// CARGAR DESDE FLASH
+void loadPreferences() {
+  COIL_AIR_PUMP = preferences.getInt("COIL_AIR_PUMP", 0);
+  HREG_LIGHT_PWM = preferences.getInt("HREG_LIGHT_PWM", 0);
+  HREG_MODE = preferences.getInt("HREG_MODE", 0);
+  HREG_LUX_SP = preferences.getInt("HREG_LUX_SP", 100);
+  HREG_AIR_ON_TIME = preferences.getInt("HREG_AIR_ON_TIME", 180);
+  HREG_AIR_OFF_TIME = preferences.getInt("HREG_AIR_OFF_TIME", 3420);
+
+  HREG_B1_SP = preferences.getInt("HREG_B1_SP", 500);
+  HREG_B1_ON_TIME = preferences.getInt("HREG_B1_ON_TIME", 10);
+  HREG_B1_OFF_TIME = preferences.getInt("HREG_B1_OFF_TIME", 300);
+
+  HREG_B2_SP = preferences.getInt("HREG_B2_SP", 500);
+  HREG_B2_ON_TIME = preferences.getInt("HREG_B2_ON_TIME", 10);
+  HREG_B2_OFF_TIME = preferences.getInt("HREG_B2_OFF_TIME", 300);
+
+  HREG_B3_SP = preferences.getInt("HREG_B3_SP", 500);
+  HREG_B3_ON_TIME = preferences.getInt("HREG_B3_ON_TIME", 10);
+  HREG_B3_OFF_TIME = preferences.getInt("HREG_B3_OFF_TIME", 300);
+}
--- esp/codeMqtt.ino
+++ esp/codeMqtt.ino
@@ -2,20 +2,11 @@
 #include <PubSubClient.h>
 #include "Adafruit_VEML7700.h"
 #include <PID_v1.h>
+#include <ArduinoJson.h>
 
 Adafruit_VEML7700 veml = Adafruit_VEML7700();
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-// === SENSORES ===
-int rawTDS, rawPH, rawTEMP;
-double lux, pwmLight, setpoint = 100;
-
-// === PID ===
-PID LUX_PID(&lux, &pwmLight, &setpoint, 0.1, 0.5, 0, DIRECT);
-
-// === PINES ===
+// Pines
 const int LSL_PIN = 39;
 const int LSH_PIN = 36;
 const int EN[4]  = {19, 4, 26, 13};
@@ -27,33 +18,61 @@ const int TEMP_PIN = 32;
 const int PWM_LIGHT_PIN = 23;
 const int AIR_PUMP_PIN = 3;
 
-// === TIMERS ===
+// Variables globales según #define
+int COIL_AIR_PUMP = 0;
+int COIL_BOMBA[4] = {0,0,0,0};
+
+int HREG_LIGHT_PWM = 0;
+int HREG_BOMBA[4] = {0,0,0,0};
+int HREG_MODE = 0;
+int HREG_LUX_SP = 100;
+int HREG_AIR_ON_TIME = 180;
+int HREG_AIR_OFF_TIME = 3420;
+int HREG_B1_SP = 500, HREG_B1_ON_TIME = 10, HREG_B1_OFF_TIME = 300;
+int HREG_B2_SP = 500, HREG_B2_ON_TIME = 10, HREG_B2_OFF_TIME = 300;
+int HREG_B3_SP = 500, HREG_B3_ON_TIME = 10, HREG_B3_OFF_TIME = 300;
+
+// Sensores
+int IREG_TDS_RAW, IREG_PH_RAW, IREG_TEMP_RAW;
+int IREG_LIGHT_LUX, IREG_LSL, IREG_LSH;
+
+// PID
+double lux, pwmLight, setpoint;
+PID LUX_PID(&lux, &pwmLight, &setpoint, 0.1, 0.5, 0, DIRECT);
+
+// TIMERS
 unsigned long t_now, t_last_air = 0, t_last_bomba[3] = {0,0,0};
 bool air_state = false, bomba_state[3] = {false, false, false};
 
-// === CONFIGURACION ===
-int mode = 0;
-int air_on_time = 180 * 1000;
-int air_off_time = 3420 * 1000;
-int bomba_sp[3] = {500, 500, 500};
-int bomba_on_time[3] = {10 * 1000, 10 * 1000, 10 * 1000};
-int bomba_off_time[3] = {300 * 1000, 300 * 1000, 300 * 1000};
-int pwm_manual = 0;
+// WiFi y MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
   Serial.begin(115200);
-
+  
   WiFi.begin("REHF-2.4G", "tontosYtorpes291");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500); Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
-  Serial.println("WiFi conectado. IP:");
+  Serial.println("\nWiFi conectado. IP:");
   Serial.println(WiFi.localIP());
 
-  client.setServer("192.168.1.100", 1883);  // Cambia por tu broker MQTT
+  client.setServer("192.168.4.4", 1883);
   client.setCallback(callback);
 
-  veml.begin();
+  while (!client.connected()) {
+    if (client.connect("ESP32Controller")) {
+      client.subscribe("controller/#");
+      client.subscribe("controller/get_state");
+    } else {
+      delay(500);
+    }
+  }
+
+  // Sensores
+  if (!veml.begin()) { while (1); }
   veml.setLowThreshold(10000);
   veml.setHighThreshold(20000);
   veml.interruptEnable(true);
@@ -61,10 +80,9 @@ void setup() {
   LUX_PID.SetOutputLimits(0, 255);
   LUX_PID.SetMode(AUTOMATIC);
 
-  for (int i=0; i<4; i++) {
-    pinMode(EN[i], OUTPUT);
-    pinMode(IN1[i], OUTPUT);
-    pinMode(IN2[i], OUTPUT);
+  // Pines
+  for (int i=0;i<4;i++) {
+    pinMode(EN[i], OUTPUT); pinMode(IN1[i], OUTPUT); pinMode(IN2[i], OUTPUT);
   }
   pinMode(AIR_PUMP_PIN, OUTPUT);
   pinMode(LSL_PIN, INPUT);
@@ -72,113 +90,107 @@ void setup() {
 }
 
 void loop() {
-  if (!client.connected()) reconnect();
   client.loop();
-
   t_now = millis();
-  
-  rawTDS = analogRead(TDS_PIN);
-  rawPH = analogRead(PH_PIN);
-  rawTEMP = analogRead(TEMP_PIN);
-  lux = veml.readLux();
-  bool LSL = digitalRead(LSL_PIN);
-  bool LSH = digitalRead(LSH_PIN);
 
-  if ((mode == 1) && (LSL == 0)) {
-    controlAutomatico(rawTDS, rawPH);
-  } else {
-    controlManual();
-  }
+  // Sensores
+  IREG_TDS_RAW = analogRead(TDS_PIN);
+  IREG_PH_RAW = analogRead(PH_PIN);
+  IREG_TEMP_RAW = analogRead(TEMP_PIN);
+  lux = veml.readLux();
+  IREG_LIGHT_LUX = (int)lux;
+  IREG_LSL = digitalRead(LSL_PIN);
+  IREG_LSH = digitalRead(LSH_PIN);
+
+  if ((HREG_MODE == 1) && (IREG_LSL==0)) controlAutomatico(IREG_TDS_RAW, IREG_PH_RAW);
+  else controlManual();
 }
 
+// CONTROL
 void controlManual() {
-  for (int i = 0; i < 4; i++) {
-    analogWrite(EN[i], pwm_manual);
-    digitalWrite(IN1[i], pwm_manual > 0);
-    digitalWrite(IN2[i], LOW);
+  for (int i=0;i<4;i++) {
+    if (COIL_BOMBA[i] && HREG_BOMBA[i] > 0) {
+      analogWrite(EN[i], HREG_BOMBA[i]);
+      digitalWrite(IN1[i], HIGH);
+      digitalWrite(IN2[i], LOW);
+    } else {
+      analogWrite(EN[i], 0);
+      digitalWrite(IN1[i], LOW);
+      digitalWrite(IN2[i], LOW);
+    }
   }
-  digitalWrite(AIR_PUMP_PIN, false);
-  analogWrite(PWM_LIGHT_PIN, pwm_manual);
+  digitalWrite(AIR_PUMP_PIN, COIL_AIR_PUMP);
+  analogWrite(PWM_LIGHT_PIN, HREG_LIGHT_PWM);
 }
 
 void controlAutomatico(int tds, int ph) {
+  setpoint = HREG_LUX_SP;
   LUX_PID.Compute();
   analogWrite(PWM_LIGHT_PIN, (int)pwmLight);
 
-  // Bomba de aire
-  if (air_state && (t_now - t_last_air >= air_on_time)) {
+  // Aire
+  if (air_state && (t_now - t_last_air >= HREG_AIR_ON_TIME*1000)) {
     air_state = false; t_last_air = t_now;
-  } else if (!air_state && (t_now - t_last_air >= air_off_time)) {
+  } else if (!air_state && (t_now - t_last_air >= HREG_AIR_OFF_TIME*1000)) {
     air_state = true; t_last_air = t_now;
   }
   digitalWrite(AIR_PUMP_PIN, air_state);
 
-  for (int i = 0; i < 3; i++) {
-    bool cond = (i < 2) ? (tds < bomba_sp[i]) : (ph < bomba_sp[i]);
-    if (bomba_state[i] && (t_now - t_last_bomba[i] >= bomba_on_time[i])) {
-      bomba_state[i] = false; t_last_bomba[i] = t_now;
-    } else if (!bomba_state[i] && cond && (t_now - t_last_bomba[i] >= bomba_off_time[i])) {
-      bomba_state[i] = true; t_last_bomba[i] = t_now;
+  // Bombas
+  int sp[3]={HREG_B1_SP,HREG_B2_SP,HREG_B3_SP};
+  int on[3]={HREG_B1_ON_TIME*1000,HREG_B2_ON_TIME*1000,HREG_B3_ON_TIME*1000};
+  int off[3]={HREG_B1_OFF_TIME*1000,HREG_B2_OFF_TIME*1000,HREG_B3_OFF_TIME*1000};
+
+  for (int i=0;i<3;i++) {
+    bool cond = (i<2) ? (tds<sp[i]) : (ph<sp[i]);
+    if (bomba_state[i] && (t_now - t_last_bomba[i] >= on[i])) {
+      bomba_state[i]=false; t_last_bomba[i]=t_now;
+    } else if (!bomba_state[i] && cond && (t_now - t_last_bomba[i] >= off[i])) {
+      bomba_state[i]=true; t_last_bomba[i]=t_now;
     }
-    int pwm = bomba_state[i] ? 255 : 0;
+    int pwm = bomba_state[i]?255:0;
     analogWrite(EN[i], pwm);
     digitalWrite(IN1[i], bomba_state[i]);
     digitalWrite(IN2[i], LOW);
   }
 }
 
-// === MQTT ===
-void callback(char* topic, byte* message, unsigned int length) {
-  String msg;
-  for (uint i = 0; i < length; i++) msg += (char)message[i];
+// MQTT
+void callback(char* topic, byte* payload, unsigned int length) {
+  String t = topic;
+  payload[length] = '\0';
+  String msg = String((char*)payload);
 
-  if (String(topic) == "ask/sensors") {
-    String json = "{";
-    json += "\"TDS\":" + String(rawTDS) + ",";
-    json += "\"PH\":" + String(rawPH) + ",";
-    json += "\"TEMP\":" + String(rawTEMP) + ",";
-    json += "\"LUX\":" + String(lux) + ",";
-    json += "\"LSL\":" + String(digitalRead(LSL_PIN)) + ",";
-    json += "\"LSH\":" + String(digitalRead(LSH_PIN));
-    json += "}";
-    client.publish("status/sensors", json.c_str());
-  }
-  else if (String(topic) == "control/mode") mode = msg.toInt();
-  else if (String(topic) == "control/lux_sp") setpoint = msg.toDouble();
-  else if (String(topic) == "control/air_on_time") air_on_time = msg.toInt() * 1000;
-  else if (String(topic) == "control/air_off_time") air_off_time = msg.toInt() * 1000;
-  else if (String(topic) == "control/b1_sp") bomba_sp[0] = msg.toInt();
-  else if (String(topic) == "control/b1_on") bomba_on_time[0] = msg.toInt() * 1000;
-  else if (String(topic) == "control/b1_off") bomba_off_time[0] = msg.toInt() * 1000;
-  else if (String(topic) == "control/b2_sp") bomba_sp[1] = msg.toInt();
-  else if (String(topic) == "control/b2_on") bomba_on_time[1] = msg.toInt() * 1000;
-  else if (String(topic) == "control/b2_off") bomba_off_time[1] = msg.toInt() * 1000;
-  else if (String(topic) == "control/b3_sp") bomba_sp[2] = msg.toInt();
-  else if (String(topic) == "control/b3_on") bomba_on_time[2] = msg.toInt() * 1000;
-  else if (String(topic) == "control/b3_off") bomba_off_time[2] = msg.toInt() * 1000;
-  else if (String(topic) == "control/pwm_manual") pwm_manual = msg.toInt();
-}
+  // Cambio directo
+  if (t == "controller/COIL_AIR_PUMP") COIL_AIR_PUMP = msg.toInt();
+  else if (t == "controller/HREG_LIGHT_PWM") HREG_LIGHT_PWM = msg.toInt();
+  else if (t == "controller/HREG_MODE") HREG_MODE = msg.toInt();
+  else if (t == "controller/HREG_LUX_SP") HREG_LUX_SP = msg.toInt();
+  else if (t == "controller/HREG_AIR_ON_TIME") HREG_AIR_ON_TIME = msg.toInt();
+  else if (t == "controller/HREG_AIR_OFF_TIME") HREG_AIR_OFF_TIME = msg.toInt();
+  else if (t == "controller/HREG_B1_SP") HREG_B1_SP = msg.toInt();
+  else if (t == "controller/HREG_B1_ON_TIME") HREG_B1_ON_TIME = msg.toInt();
+  else if (t == "controller/HREG_B1_OFF_TIME") HREG_B1_OFF_TIME = msg.toInt();
+  else if (t == "controller/HREG_B2_SP") HREG_B2_SP = msg.toInt();
+  else if (t == "controller/HREG_B2_ON_TIME") HREG_B2_ON_TIME = msg.toInt();
+  else if (t == "controller/HREG_B2_OFF_TIME") HREG_B2_OFF_TIME = msg.toInt();
+  else if (t == "controller/HREG_B3_SP") HREG_B3_SP = msg.toInt();
+  else if (t == "controller/HREG_B3_ON_TIME") HREG_B3_ON_TIME = msg.toInt();
+  else if (t == "controller/HREG_B3_OFF_TIME") HREG_B3_OFF_TIME = msg.toInt();
 
-void reconnect() {
-  while (!client.connected()) {
-    if (client.connect("ESP32_Client")) {
-      client.subscribe("ask/sensors");
-      client.subscribe("control/mode");
-      client.subscribe("control/lux_sp");
-      client.subscribe("control/air_on_time");
-      client.subscribe("control/air_off_time");
-      client.subscribe("control/b1_sp");
-      client.subscribe("control/b1_on");
-      client.subscribe("control/b1_off");
-      client.subscribe("control/b2_sp");
-      client.subscribe("control/b2_on");
-      client.subscribe("control/b2_off");
-      client.subscribe("control/b3_sp");
-      client.subscribe("control/b3_on");
-      client.subscribe("control/b3_off");
-      client.subscribe("control/pwm_manual");
-    } else {
-      delay(5000);
-    }
+  // get_state
+  else if (t == "controller/get_state") {
+    StaticJsonDocument<512> doc;
+    doc["TDS"] = IREG_TDS_RAW;
+    doc["PH"] = IREG_PH_RAW;
+    doc["TEMP"] = IREG_TEMP_RAW;
+    doc["LUX"] = IREG_LIGHT_LUX;
+    doc["LSL"] = IREG_LSL;
+    doc["LSH"] = IREG_LSH;
+    doc["AIR"] = COIL_AIR_PUMP;
+    doc["PWM_LIGHT"] = HREG_LIGHT_PWM;
+    char out[512];
+    serializeJson(doc, out);
+    client.publish("controller/state", out);
   }
 }
